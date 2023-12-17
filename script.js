9fe1765b849394dead3d07760de341d65467da6f
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2023-12-17 14:21:10 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+// Get references to password input fields and eye icons
 let pass1eye = document.getElementById("pass1eye");
 let pass2eye = document.getElementById("pass2eye");
 
 let password1 = document.getElementById("password1");
 let password2 = document.getElementById("password2");
 
-
-//if password1 is not empty, show the eye icon
+// Event listener for keyup on password1 input
 password1.addEventListener("keyup", function () {
+    // Show or hide the eye icon based on whether password1 is empty
     if (password1.value != "") {
         pass1eye.style.display = "block";
     } else {
@@ -14,8 +15,9 @@ password1.addEventListener("keyup", function () {
     }
 });
 
-//if password2 is not empty, show the eye icon
+// Event listener for keyup on password2 input
 password2.addEventListener("keyup", function () {
+    // Show or hide the eye icon based on whether password2 is empty
     if (password2.value != "") {
         pass2eye.style.display = "block";
     } else {
@@ -23,8 +25,9 @@ password2.addEventListener("keyup", function () {
     }
 });
 
-
+// Event listener for click on pass1eye (eye icon for password1)
 pass1eye.addEventListener("click", function () {
+    // Toggle the visibility of password1 and update the eye icon
     if (password1.type === "password") {
         password1.type = "text";
         pass1eye.classList.remove("bi-eye");
@@ -36,11 +39,12 @@ pass1eye.addEventListener("click", function () {
     }
 });
 
+// Event listener for click on pass2eye (eye icon for password2)
 pass2eye.addEventListener("click", function () {
+    // Toggle the visibility of password2
     if (password2.type === "password") {
         password2.type = "text";
     } else {
         password2.type = "password";
     }
 });
-
